6d04db8abe35b312edf022ad4827d7c4a6ab1156
k-theorydotcom <contact@k-theory.com>
2016-10-26 16:00:40 -0700

    adding if/else

--- wrap-display-plugin.php
+++ wrap-display-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
 * Plugin Name: Wrap Display Plugin
-* Description: Create your WordPress shortcode.
-* Version: 1.0
+* Description: Add Wraps inline, embeded, linked by using [wrap id=xxx].
+* Version: 0.2
 * Author: Jeff Klein
 * Author URI: Wrap.co
 */
@@ -12,26 +12,37 @@
 add_action( 'wp_enqueue_scripts', 'wrap_display_styles' );
 
 function wrap_display_styles() {
+   
    wp_enqueue_style( 'main', plugins_url( '/css/main.css', __FILE__ ));
    
    wp_enqueue_script( 'embed', 'https://wrap.co/embed/bootstrap.js', true, true );
    //wp_enqueue_script( $handle, $src = false, $deps = array(), $ver = false, $in_footer = false )
-
 }
 
 
 // [bartag foo="foo-value"]
 function wrap_display( $atts ) {
     $a = shortcode_atts( array(
-        'id' => 'something',
+        'id' => '2a76c122-0b94-40c7-b3d8-2ee7925a6e77',
         'public' => 'something else',
+        'phone' => false,
+        'theme' => 'none',
     ), $atts );
 
-    //return "foo = {$a['id']}";
+  
     
 	
-	//return "<a href='https://wrap.co/wraps/" . $a['id'] . "' target='_blank'><img src='https://assets.wrap.co/covers/" . $a['id'] . "/html_email/default' class='wrap-embed'/></a>";
-	return "<figure class='animated-overlay overlay-style caption-hover clearfix wrap-media-embed-overlay'><a class='img-link' href='https://wrap.co/wraps/" . $a['id'] . "' target='_self'></a><div class='wrap-display-card img-wrap' data-id=" . $a['id'] . "><img src='https://assets.wrap.co/covers/" . $a['id'] . "/embed/half_default' class='wrap-embed'/></div><figcaption><div class='thumb-info'><h5>View Wrap</h5></div></figcaption></figure>";
+	if ($a['theme'] == 'cardinal') { 
+		return "<figure class='animated-overlay overlay-style caption-hover clearfix wrap-media-embed-overlay'><a class='img-link' href='https://wrap.co/wraps/" . $a['id'] . "' target='_self'></a><div class='wrap-display-card img-wrap' data-id=" . $a['id'] . "><img src='https://assets.wrap.co/covers/" . $a['id'] . "/embed/default' class='wrap-embed'/></div><figcaption><div class='thumb-info'><h5>View Wrap</h5></div></figcaption></figure>";
+	}
+	
+	if ($a['phone'] == true) {
+		
+	}
+	
+	else {
+		return "<a href='https://wrap.co/wraps/" . $a['id'] . "' target='_blank'><img src='https://assets.wrap.co/covers/" . $a['id'] . "/embed/default' class='wrap-embed'/></a>";
+	}
 			
 			
     
@@ -41,4 +52,3 @@ add_shortcode( 'wrap', 'wrap_display' );
 
 
 ?>
-
